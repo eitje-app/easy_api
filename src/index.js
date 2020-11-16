b9143780797c41aa62a96ab3a7d261c76ecbfe95
Abel van Hoek <abelvanhoek@gmail.com>
2020-11-16 17:56:36 +0100

    use api instead of bakend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import {setup, config} from './config'
-import initApi, {backend} from './backend'
+import initApi, {api} from './backend'
 import * as API from './api'
 import reducer from './reducer'
+const backend = api;
 export {setup, initApi, API, backend, config, reducer}
 
 Object.assign(module.exports, require('./selectors'));
