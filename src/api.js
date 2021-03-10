d8069943d2cfe62edc3ea65ef72b771b5a13ad71
Abel van Hoek <abelvanhoek@gmail.com>
2021-03-10 16:04:43 +0100

    prevent crash in byIds

--- src/api.js
+++ src/api.js
@@ -286,7 +286,7 @@ export async function toggle(kind, params, {extraParams = {}} = {}) {
 export async function getByIds(kind, ids, extraParams = {}) {
   const url = `${kind}/by_ids`
   const res = await backend.get(url, {ids})
-  if (res.ok) {
+  if (res.ok && res.data) {
     const {items} = res.data
     createMultiLocal(kind, items, extraParams)
   }
