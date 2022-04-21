ce45ac877cdcc4d3a2b3b0e7344053ae7c6e6af2
Abel van Hoek <abelvanhoek@gmail.com>
2022-04-21 12:41:03 +0200

    use _.isPlainObject, the easiest check

--- src/selectors.js
+++ src/selectors.js
@@ -55,7 +55,7 @@ export const all = createCachedSelector(
 )({keySelector: (state, key, opts = {}) => `${key}-${JSON.stringify(opts)}`, selectorCreator: createDeepEqualSelector})
 
 const _buildRecords = (ents, key, opts) => {
-  const records = ents && ents.hasOwnProperty(key) ? ents : {[key]: ents} // deletedStamps is always present, tells us if we hae all ents or just a slice. This is needed for findRecords' performance
+  const records = ents && _.isPlainObject(ents) ? ents : {[key]: ents} // deletedStamps is always present, tells us if we hae all ents or just a slice. This is needed for findRecords' performance
   return buildRecords(records, key, opts) || []
 }
 
