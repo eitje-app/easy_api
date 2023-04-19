a72e3dea35675f72c6669c1f777a0fe1f8901e32
Christiaan <ckreeftmeijer@gmail.com>
2023-04-19 12:11:45 +0200

    fix exports to work with local connecting to eitje web

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {setup, config} from './config'
-import initApi, {api as backend} from './backend'
-import * as API from './api'
-import reducer from './reducer'
-import apiReducer from './api_reducer'
+import { setup, config } from "./config";
+import initApi, { api as backend } from "./backend";
+import * as API from "./api";
+import reducer from "./reducer";
+import apiReducer from "./api_reducer";
 
-export {setup, initApi, API, backend, config, reducer, apiReducer}
-Object.assign(module.exports, require('./selectors'))
-Object.assign(module.exports, require('./actions'))
-Object.assign(module.exports, require('./hooks'))
+export { setup, initApi, API, backend, config, reducer, apiReducer };
+export * from "./selectors";
+export * from "./actions";
+export * from "./hooks";
