4a50b4f0d632e75b9b6c760eb1355667471820bf
Abel van Hoek <abel@eitje.app>
2023-08-14 17:17:17 +0200

    allow joins with find

--- src/hooks/use_find.js
+++ src/hooks/use_find.js
@@ -2,9 +2,9 @@ import React, {useState, useEffect, Fragment} from 'react'
 import {useSelector} from 'react-redux'
 import {find} from '../index'
 
-export const useFind = (kind, id) => {
-  const item = useSelector(state => find(state, kind, id ))
-  return item;
+export const useFind = (kind, id, opts) => {
+  const item = useSelector((state) => find(state, kind, id, opts))
+  return item
 }
 
 export default useFind
