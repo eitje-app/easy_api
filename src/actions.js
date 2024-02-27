58c8ca77b7b95c91c63e509d63bef43766f6c2e8
Christiaan <ckreeftmeijer@gmail.com>
2024-02-27 15:29:44 +0100

    run prettier on master

--- src/actions.js
+++ src/actions.js
@@ -1,98 +1,106 @@
-import _ from 'lodash'
-import moment from 'moment'
+import _ from "lodash";
+import moment from "moment";
 
 export const includesRecord = (entities, query) => {
   return entities.filter((e) => {
-    const keys = Object.keys(query) // [1,2,3,4]
-    return keys.every((k) => query[k].some((id) => e[k] && e[k].includes(id)))
-  })
-}
+    const keys = Object.keys(query); // [1,2,3,4]
+    return keys.every((k) => query[k].some((id) => e[k] && e[k].includes(id)));
+  });
+};
 
-export const filterByDate = (entities, {start, end}, {dateField = 'date'} = {}) => {
+export const filterByDate = (
+  entities,
+  { start, end },
+  { dateField = "date" } = {}
+) => {
   return entities.filter((e) => {
-    if (start && end) return e[dateField] >= start && e[dateField] <= end
-    if (start) return e[dateField] >= start
-    return e[dateField] <= end
-  })
-}
+    if (start && end) return e[dateField] >= start && e[dateField] <= end;
+    if (start) return e[dateField] >= start;
+    return e[dateField] <= end;
+  });
+};
 
 export const findRecord = (entities, query) => {
   return entities.find((e) => {
-    if (!_.isObject(query)) return e.id == Number(query)
-    const keys = Object.keys(query)
-    return filterKeys(keys, query, e)
-  })
-}
+    if (!_.isObject(query)) return e.id == Number(query);
+    const keys = Object.keys(query);
+    return filterKeys(keys, query, e);
+  });
+};
 
 // exact: literally compare both values, no array/range magic
 
 export const filterRecords = (entities, query, options) => {
   return entities.filter((e) => {
-    if (_.isArray(query)) return query.includes(e.id)
-    if (_.isNumber(query)) return Number(query) == e.id
-    const keys = Object.keys(query)
-    return filterKeys(keys, query, e, options)
-  })
-}
+    if (_.isArray(query)) return query.includes(e.id);
+    if (_.isNumber(query)) return Number(query) == e.id;
+    const keys = Object.keys(query);
+    return filterKeys(keys, query, e, options);
+  });
+};
 
 export const inverseFilterRecords = (entities, query) => {
   return entities.filter((e) => {
-    const keys = Object.keys(query)
-    return !filterKeys(keys, query, e)
-  })
-}
+    const keys = Object.keys(query);
+    return !filterKeys(keys, query, e);
+  });
+};
 
 const filterKeys = (keys, query, record, options) => {
   return keys.every((k) => {
-    const queryVal = query[k]
-    const recordVal = record[k]
-    return filterRecord(queryVal, recordVal, options)
-  })
-}
+    const queryVal = query[k];
+    const recordVal = record[k];
+    return filterRecord(queryVal, recordVal, options);
+  });
+};
 
-export const filterRecord = (queryVal, recordVal, {exact} = {}) => {
-  const args = [queryVal, recordVal]
-  queryVal = sanitizeVal(queryVal)
-  recordVal = sanitizeVal(recordVal)
-  if (!exact && hasArray(args)) return filterArrays(queryVal, recordVal)
-  if (hasRange(args)) return filterRanges(queryVal, recordVal)
-  return _.isEqual(queryVal, recordVal)
-}
+export const filterRecord = (queryVal, recordVal, { exact } = {}) => {
+  const args = [queryVal, recordVal];
+  queryVal = sanitizeVal(queryVal);
+  recordVal = sanitizeVal(recordVal);
+  if (!exact && hasArray(args)) return filterArrays(queryVal, recordVal);
+  if (hasRange(args)) return filterRanges(queryVal, recordVal);
+  return _.isEqual(queryVal, recordVal);
+};
 
-const hasRange = (arr) => arr.some((i) => isRange(i))
-const isRange = (i) => _.isObject(i) && i.start && i.start instanceof moment
-const hasArray = (arr) => arr.some((i) => _.isArray(i))
+const hasRange = (arr) => arr.some((i) => isRange(i));
+const isRange = (i) => _.isObject(i) && i.start && i.start instanceof moment;
+const hasArray = (arr) => arr.some((i) => _.isArray(i));
 
 const filterRanges = (queryVal, recordVal) => {
-  const args = [queryVal, recordVal]
-  if (isRange(queryVal) && isRange(recordVal)) return queryVal.overlaps(recordVal)
-  const stringVal = args.find((a) => _.isString(a)) // dates are always sent to us as strings
-  const rangeVal = args.find((a) => isRange(a))
-  if (!rangeVal || !stringVal) return
-  const momentized = moment(stringVal)
+  const args = [queryVal, recordVal];
+  if (isRange(queryVal) && isRange(recordVal))
+    return queryVal.overlaps(recordVal);
+  const stringVal = args.find((a) => _.isString(a)); // dates are always sent to us as strings
+  const rangeVal = args.find((a) => isRange(a));
+  if (!rangeVal || !stringVal) return;
+  const momentized = moment(stringVal);
 
   if (rangeVal.isSingleDay()) {
-    return rangeVal.start.format() == stringVal || rangeVal.end.format() == stringVal
+    return (
+      rangeVal.start.format() == stringVal || rangeVal.end.format() == stringVal
+    );
   }
 
-  return rangeVal.contains(momentized)
-}
+  return rangeVal.contains(momentized);
+};
 
 const sanitizeVal = (val) => {
-  if (val instanceof moment) return val.format('YYYY-MM-DD')
-  if (_.isString(val)) return sanitizeString(val)
-  return val
-}
+  if (val instanceof moment) return val.format("YYYY-MM-DD");
+  if (_.isString(val)) return sanitizeString(val);
+  return val;
+};
 
 const sanitizeString = (val) => {
-  if (!isNaN(val)) return Number(val)
-  return val
-}
+  if (!isNaN(val)) return Number(val);
+  return val;
+};
 
 function filterArrays(arr1, arr2) {
-  if (_.isArray(arr1) && _.isArray(arr2)) return _.intersection(arr1, arr2).length > 0
-  if (_.isArray(arr1)) return arr1.includes(arr2)
-  if (_.isArray(arr2)) return arr2.includes(arr1)
+  if (_.isArray(arr1) && _.isArray(arr2))
+    return _.intersection(arr1, arr2).length > 0;
+  if (_.isArray(arr1)) return arr1.includes(arr2);
+  if (_.isArray(arr2)) return arr2.includes(arr1);
 }
 
-const numberOrString = (val) => _.isNumber(val) || _.isString(val)
+const numberOrString = (val) => _.isNumber(val) || _.isString(val);
--- src/selectors.js
+++ src/selectors.js
@@ -1,18 +1,31 @@
-import {createSelector, createSelectorCreator, defaultMemoize} from 'reselect'
-import createCachedSelector from 're-reselect'
-import _ from 'lodash'
-import moment from 'moment'
-import utils from '@eitje/utils'
-import {findRecord, inverseFilterRecords, filterRecords, includesRecord, filterByDate} from './actions'
-import {config} from './config'
-import pluralize from 'pluralize'
-import joins, {checkMultiple} from './joins'
+import {
+  createSelector,
+  createSelectorCreator,
+  defaultMemoize,
+} from "reselect";
+import createCachedSelector from "re-reselect";
+import _ from "lodash";
+import moment from "moment";
+import utils from "@eitje/utils";
+import {
+  findRecord,
+  inverseFilterRecords,
+  filterRecords,
+  includesRecord,
+  filterByDate,
+} from "./actions";
+import { config } from "./config";
+import pluralize from "pluralize";
+import joins, { checkMultiple } from "./joins";
 
-const sanitizeKind = (kind) => pluralize(utils.snakeToCamel(kind))
+const sanitizeKind = (kind) => pluralize(utils.snakeToCamel(kind));
 
-const authUserSelector = (state) => state.auth.user
-const usersSelector = (state) => state.records.users
-const createDeepEqualSelector = createSelectorCreator(defaultMemoize, _.isEqual)
+const authUserSelector = (state) => state.auth.user;
+const usersSelector = (state) => state.records.users;
+const createDeepEqualSelector = createSelectorCreator(
+  defaultMemoize,
+  _.isEqual
+);
 
 // 217 > 101
 // ({[key]: records[key]}): 217
@@ -20,141 +33,163 @@ const createDeepEqualSelector = createSelectorCreator(defaultMemoize, _.isEqual)
 // records[key]: 41
 // state.records: 101
 
-const allowedOpts = ['joins']
+const allowedOpts = ["joins"];
 
 const sanitizeOpts = (opts) => {
-  if (!_.isObject(opts)) return null
-  if (!utils.intersects(Object.keys(opts), allowedOpts)) return null
-  return opts
-}
+  if (!_.isObject(opts)) return null;
+  if (!utils.intersects(Object.keys(opts), allowedOpts)) return null;
+  return opts;
+};
 
 const getModel = (key) => {
-  const model = config.models[key]
-  return {model, defaultJoins: model?.defaultJoins}
-}
+  const model = config.models[key];
+  return { model, defaultJoins: model?.defaultJoins };
+};
 
 const findRecords = (state, kind, opts = {}) => {
-  kind = sanitizeKind(kind)
-  const {model, defaultJoins} = getModel(kind)
-  if (!opts) opts = {}
+  kind = sanitizeKind(kind);
+  const { model, defaultJoins } = getModel(kind);
+  if (!opts) opts = {};
   if (defaultJoins) {
-    opts = {...opts, joins: utils.composeArray(opts.joins, defaultJoins)}
+    opts = { ...opts, joins: utils.composeArray(opts.joins, defaultJoins) };
   }
 
   if (utils.exists(opts.joins)) {
-    return _.pick(state.records, [kind, ...opts.joins])
+    return _.pick(state.records, [kind, ...opts.joins]);
   }
 
-  return state.records[kind]
-}
+  return state.records[kind];
+};
 
 export const all = createCachedSelector(
   findRecords,
   (state, key) => sanitizeKind(key),
   (state, key, opts) => sanitizeOpts(opts),
-  (ents, key, opts) => _buildRecords(ents, key, opts),
-)({keySelector: (state, key, opts = {}) => `${key}-${JSON.stringify(opts)}`, selectorCreator: createDeepEqualSelector})
+  (ents, key, opts) => _buildRecords(ents, key, opts)
+)({
+  keySelector: (state, key, opts = {}) => `${key}-${JSON.stringify(opts)}`,
+  selectorCreator: createDeepEqualSelector,
+});
 
 const _buildRecords = (ents, key, opts) => {
-  const records = ents && _.isPlainObject(ents) ? ents : {[key]: ents}
-  const finalRecords = buildRecords(records, key, opts) || []
+  const records = ents && _.isPlainObject(ents) ? ents : { [key]: ents };
+  const finalRecords = buildRecords(records, key, opts) || [];
 
-  return finalRecords
-}
+  return finalRecords;
+};
 
-const defaultArr = []
+const defaultArr = [];
 
 const buildRecords = (ents = {}, key, opts = {}) => {
   // console.log(`All. key: ${key}`, ents, opts)
-  if (!_.isObject(opts)) opts = {}
-  const {model, defaultJoins} = getModel(key)
-  const joinKeys = utils.composeArray(opts.joins, defaultJoins)
-  let final = enrichRecords(ents, key) || defaultArr
+  if (!_.isObject(opts)) opts = {};
+  const { model, defaultJoins } = getModel(key);
+  const joinKeys = utils.composeArray(opts.joins, defaultJoins);
+  let final = enrichRecords(ents, key) || defaultArr;
 
   joinKeys.forEach((k) => {
-    const mergeItems = enrichRecords(ents, k)
-    final = joins({items: final, mergeItems, tableName: key, mergeTableName: k})
-  })
+    const mergeItems = enrichRecords(ents, k);
+    final = joins({
+      items: final,
+      mergeItems,
+      tableName: key,
+      mergeTableName: k,
+    });
+  });
 
-  const assoc = config.createAssociation(final.map((i) => buildFullRecord(i, key, joinKeys)))
-  return assoc
-}
+  const assoc = config.createAssociation(
+    final.map((i) => buildFullRecord(i, key, joinKeys))
+  );
+  return assoc;
+};
 
 const buildFullRecord = (item, key, joinKeys) => {
-  const record = buildClassRecord(item, key)
+  const record = buildClassRecord(item, key);
   joinKeys.forEach((joinKey) => {
-    const isMultiple = checkMultiple(key, joinKey)
-    const actualJoinKey = isMultiple ? pluralize.plural(joinKey) : pluralize.singular(joinKey)
+    const isMultiple = checkMultiple(key, joinKey);
+    const actualJoinKey = isMultiple
+      ? pluralize.plural(joinKey)
+      : pluralize.singular(joinKey);
     if (record[actualJoinKey]) {
       record[actualJoinKey] = isMultiple
-        ? config.createAssociation(record[actualJoinKey].map((i) => buildClassRecord(i, actualJoinKey)))
-        : buildClassRecord(record[actualJoinKey], actualJoinKey)
+        ? config.createAssociation(
+            record[actualJoinKey].map((i) => buildClassRecord(i, actualJoinKey))
+          )
+        : buildClassRecord(record[actualJoinKey], actualJoinKey);
     }
-  })
+  });
 
-  return record
-}
+  return record;
+};
 
 const buildClassRecord = (item, key) => {
-  key = pluralize.plural(key)
-  const {model} = getModel(key)
-  return model ? new model(item) : item
-}
+  key = pluralize.plural(key);
+  const { model } = getModel(key);
+  return model ? new model(item) : item;
+};
 
 const enrichRecords = (ents, key) => {
-  const val = config.enrichRecords(ents, key) || ents[key]
-  return val
-}
+  const val = config.enrichRecords(ents, key) || ents[key];
+  return val;
+};
 
-const allExternal = (state, key, query, opts) => (utils.exists(opts) ? all(state, key, opts) : all(state, key))
+const allExternal = (state, key, query, opts) =>
+  utils.exists(opts) ? all(state, key, opts) : all(state, key);
 
 export const selfSelector = createSelector(
   authUserSelector,
-  (state) => all(state, 'users'),
-  (user, users = []) => users.find((u) => u.id === user.id) || user,
-)
+  (state) => all(state, "users"),
+  (user, users = []) => users.find((u) => u.id === user.id) || user
+);
 
 export const find = createCachedSelector(
   allExternal,
   (state, key) => key,
   (state, key, query) => query,
-  (records, key, query) => findRecord(records, query) || {},
-)({keySelector: (state, key, query) => `${key}-${JSON.stringify(query)}`, selectorCreator: createDeepEqualSelector})
+  (records, key, query) => findRecord(records, query) || {}
+)({
+  keySelector: (state, key, query) => `${key}-${JSON.stringify(query)}`,
+  selectorCreator: createDeepEqualSelector,
+});
 
 export const includes = createCachedSelector(
   allExternal,
   (state, key) => key,
   (state, key, query) => query,
-  (records, key, query) => includesRecord(records, query) || [],
-)((state, key, query) => `${key}-${JSON.stringify(query)}`)
+  (records, key, query) => includesRecord(records, query) || []
+)((state, key, query) => `${key}-${JSON.stringify(query)}`);
 
 export const where = createCachedSelector(
   allExternal,
   (state, key) => key,
   (state, key, query) => query,
-  (state, key, query, opts) => opts || '',
+  (state, key, query, opts) => opts || "",
   (records, key, query, opts) => {
-    const val = query ? filterRecords(records, query, opts) : records || []
-    return val
-  },
-)({keySelector: (state, key, query, opts) => `${key}-${JSON.stringify(query)}-${opts}`, selectorCreator: createDeepEqualSelector})
+    const val = query ? filterRecords(records, query, opts) : records || [];
+    return val;
+  }
+)({
+  keySelector: (state, key, query, opts) =>
+    `${key}-${JSON.stringify(query)}-${opts}`,
+  selectorCreator: createDeepEqualSelector,
+});
 
 export const whereNot = createCachedSelector(
   allExternal,
   (state, key) => key,
   (state, key, query) => query,
-  (records, key, query) => inverseFilterRecords(records, query) || [],
-)((state, key, query) => `${key}-${JSON.stringify(query)}`)
+  (records, key, query) => inverseFilterRecords(records, query) || []
+)((state, key, query) => `${key}-${JSON.stringify(query)}`);
 
 export const betweenDays = createCachedSelector(
   allExternal,
   (state, key) => key,
   (state, key, query) => query,
-  (records, key, query) => filterByDate(records, query),
-)((state, key, query) => `${key}-${JSON.stringify(query)}`)
+  (records, key, query) => filterByDate(records, query)
+)((state, key, query) => `${key}-${JSON.stringify(query)}`);
 
 export const afterToday = createCachedSelector(
   allExternal,
   (state, key) => key,
-  (records) => filterByDate(records, {start: moment().format('YYYY-MM-DD')}),
-)((state, key) => key)
+  (records) => filterByDate(records, { start: moment().format("YYYY-MM-DD") })
+)((state, key) => key);
