44d75b72d886995ae85678aa8129b1c4115cd686
Abel <abel@eitje.app>
2025-02-06 13:45:18 +0100

    add watch arg to useIndex

--- src/hooks/use_index.js
+++ src/hooks/use_index.js
@@ -2,13 +2,12 @@ import {API} from '../index'
 import {useEffect} from 'react'
 import useAll from './use_all'
 
-export const useIndex = (kind, opts = {}) => {
-  useEffect(() => {
-    API.index(kind, opts)
-  }, [])
-  const items = useAll(kind)
-  return items;
-
+export const useIndex = (kind, opts = {}, {watch} = {}) => {
+	useEffect(() => {
+		API.index(kind, opts)
+	}, watch)
+	const items = useAll(kind)
+	return items
 }
 
 export default useIndex
